79ff5a6d1ecbfffed9720456e36be06139608b48
DiogoBrito21 <diogo.brito@sitiorecantodoqueijo.com.br>
2025-01-08 11:02:36 -0300

    Initial commit

---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_banco_douro/Service/account_service.dart';
+import 'package:flutter_banco_douro/models/account.dart';
+import 'package:flutter_banco_douro/ui/styles/colors.dart';
+import 'package:flutter_banco_douro/ui/widgets/account_widgets.dart';
+import 'package:flutter_banco_douro/ui/widgets/add_account_modal.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<List<Account>> _futureGetAll = AccountService().getAll();
+
+  Future<void> refreshGetAll() async {
+    setState(() {
+      _futureGetAll = AccountService().getAll();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColor.lightGrey,
+        title: const Text("Sistema de gestão de contas"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, "login");
+            },
+            icon: const Icon(Icons.logout),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isScrollControlled: true,
+            builder: (context) {
+              return const AddAccountModal();
+            },
+          );
+        },
+        backgroundColor: AppColor.orange,
+        child: const Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: RefreshIndicator(
+          onRefresh: refreshGetAll,
+          child: FutureBuilder(
+            future: _futureGetAll,
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return const Center(child: CircularProgressIndicator());
+                case ConnectionState.waiting:
+                  return const Center(child: CircularProgressIndicator());
+                case ConnectionState.active:
+                  return const Center(child: CircularProgressIndicator());
+                case ConnectionState.done:
+                  {
+                    if (snapshot.data == null || snapshot.data!.isEmpty) {
+                      
+                      print("\n\n\n\   testes ${snapshot.data}");
+                      return const Center(
+                          child: Text("Nenhuma conta recebida."));
+                    } else {
+                      List<Account> listAccounts = snapshot.data!;
+                      return ListView.builder(
+                        itemCount: listAccounts.length,
+                        itemBuilder: (context, index) {
+                          Account account = listAccounts[index];
+                          return AccountWidget(account: account);
+                        },
+                      );
+                    }
+                  }
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
